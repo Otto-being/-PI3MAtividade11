17ebeec688f506d438697eb0b2856f9e209fa7a6
Otto-being <almeida.otavio@escolar.ifrn.edu.br>
2026-05-28 23:11:48 -0300

    Create multiplica.py

---
+++ multiplica.py
@@ -0,0 +1,9 @@
+def multiplicar(a, b):
+    return a * b
+
+
+x = float(input("Digite o primeiro valor: "))
+y = float(input("Digite o segundo valor: "))
+
+resultado = multiplicar(x, y)
+print("Resultado da multiplicação:", resultado)
